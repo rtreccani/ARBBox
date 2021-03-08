cda032377215096a0ae0007f74712baae5d56ec4
Raphael Treccani-Chinelli <raphael.treccanichinelli@gmail.com>
2021-03-08 10:24:01 +0000

    finished demo for now

--- ddr3_build2.srcs/sources_1/new/userland.sv
+++ ddr3_build2.srcs/sources_1/new/userland.sv
@@ -6,108 +6,34 @@ module userland(
 	input clk
 );
 
-reg [7:0] address;
+reg [31:0] counter;
 
-heartbeat #(.CLKFREQ(81250000)) h(
-	.clk(clk),
-	.beat(io.sys_led[7])
-);
-
-typedef enum {
-	IDLE,
-	ONEIN,
-	TWOIN,
-	WRITE,
-	READ,
-	READ_DELAY
-} state_t;
-
-(* fsm_encoding = "one_hot" *) state_t currentState;
-state_t nextState;
-
-
-always @(posedge clk) begin
-	currentState <= nextState;
-	case (currentState)
-		IDLE : io.sys_led[5:0] =       'b000001;
-		ONEIN : io.sys_led[5:0] =      'b000010;
-		TWOIN : io.sys_led[5:0] =      'b000011;
-		WRITE : io.sys_led[5:0] =      'b000100;
-		READ : io.sys_led[5:0] =       'b000101;
-		READ_DELAY : io.sys_led[5:0] = 'b000110;
-		default : io.sys_led[5:0] =    'b111111;
-	endcase
-end
-
-reg [7:0] FIFO [3];
+reg [23:0] val;
 
 initial begin 
-	currentState <= IDLE;
-	nextState <= IDLE;
+	val <= 'b100110101100111001101111;
+	counter <= 'b0;
 end
 
-assign io.led = {FIFO[2], FIFO[1], FIFO[0]};
+wire [47:0] tmp;
+assign io.led = val;
+assign tmp = (val*val);
 
 always @(posedge clk) begin
-
-	//sensible defaults 
-	ddr.wr_valid <= 'b0;
-	ddr.rd_cmd_valid <= 'b0;
-	usb.newDataOut <= 'b0;
-	currentState <= nextState;
+	counter <= counter + 1;
+	if(counter > {io.dip, 3'b1}) begin
+		if(val == (tmp[35:12])) begin
+			val <= ~(tmp[35:12]);
+		end else begin
+			val <= tmp[35:12]; //make an oppenheimer PRNG
+		end
+		counter <= 0;
+	end
 	
-	
-	case(currentState)
-		IDLE : begin
-			if(usb.newDataIn) begin
-				FIFO[0] <= usb.dataIn;
-				nextState <= ONEIN;
-			end
-		end
-		
-		ONEIN : begin
-			if(usb.newDataIn) begin
-				if(FIFO[0] == 'h52) begin //read command
-					nextState <= READ;
-				end else begin
-					FIFO[1] <= usb.dataIn;
-					nextState <= TWOIN;
-				end
-			end
-		end
-		
-		TWOIN : begin
-			if(usb.newDataIn) begin
-				FIFO[2] <= usb.dataIn;
-				if(FIFO[0] == 'h57) begin
-					nextState <= WRITE;
-				end else begin
-					nextState <= IDLE;
-				end
-			end
-		end
-		
-		WRITE : begin
-			ddr.wr_addr <= {19'b0, FIFO[1]};
-			ddr.wr_data <= FIFO[2];
-			ddr.wr_valid <= 'b1;
-			nextState <= IDLE;
-		end
-		
-		READ : begin
-			ddr.rd_addr <= {19'b0, FIFO[1]};
-			ddr.rd_cmd_valid <= 'b1;
-			nextState <= READ_DELAY;
-		end
-		
-		READ_DELAY : begin
-			if(ddr.rd_data_valid) begin
-				usb.dataOut <= ddr.rd_data;
-				usb.newDataOut <= 'b1;
-				nextState <= IDLE;
-			end
-		end
-	endcase	
+	if(rst) begin
+		val <= 'b100110101100111001101111;
+		counter <= 'b0;
+	end
 end
 
 endmodule
